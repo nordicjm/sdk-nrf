b3e3f097c45ed86ca0b4d33f405d5d6ffa2b16ef
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2022-11-09 13:41:21 +0000

    Version

--- cmake/gen_fw_info.cmake
+++ cmake/gen_fw_info.cmake
@@ -2,5 +2,5 @@ if(SYSBUILD)
   # Store details for zip firmware generation in cache
   set(PM_ZIP_FW_BIN_FILE  "${PROJECT_BINARY_DIR}/${KERNEL_BIN_NAME}" CACHE STRING "" FORCE)
   set(PM_ZIP_FW_LOAD_OFFSET  "${CONFIG_FLASH_LOAD_OFFSET}" CACHE STRING "" FORCE)
-#version needed also
+  set(PM_ZIP_FW_VERSION  "${CONFIG_MCUBOOT_IMAGE_VERSION}" CACHE STRING "" FORCE)
 endif()
