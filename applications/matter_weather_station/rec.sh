90fc3dbc940c09e0dff4d3dff129dcc0e6feb7a2
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2023-07-03 14:20:16 +0100

    Relocate bluetooth

--- applications/matter_weather_station/rec.sh
+++ applications/matter_weather_station/rec.sh
@@ -2,6 +2,6 @@ nrfjprog -f NRF53 --recover
 nrfjprog -f NRF53 --coprocessor CP_NETWORK --recover
 nrfjprog -f NRF53 --coprocessor CP_NETWORK --sectorerase --program ./multiprotocol_rpmsg/zephyr/merged_CPUNET.hex
 nrfjprog -f NRF53 --sectorerase --program ./mcuboot/zephyr/zephyr.hex
-nrfjprog -f NRF53 --sectorerase --program ./intflash_signed.hex
-nrfjprog -f NRF53 --qspisectorerase --program ./qspi.hex --qspiini ../Qspi.ini --verify
+nrfjprog -f NRF53 --sectorerase --program ./zephyr/internal_flash_signed.hex
+nrfjprog -f NRF53 --qspisectorerase --program ./zephyr/qspi_flash_signed.hex --qspiini ../Qspi.ini --verify
 nrfjprog -f NRF53 --reset
