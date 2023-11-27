nordicjm/sdk-nrf

commit 8b73132c622d411ae6be54ffa9d0d6995f914505

drivers: wifi: Add binary patch download support
This remove C header based patch support and adds binary file based
patch support.

This is in preparation for storing binary patch in external flash.

Implements SHEL-2110.

Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

drivers/wifi/nrf700x/src/fw_load.c
@@ -23,32 +23,65 @@
 #endif /* CONFIG_NRF_WIFI_PATCHES_EXT_FLASH */
 
 #include <fmac_main.h>
-#include <rpu_fw_patches.h>
+
+/* INCBIN macro Taken from https://gist.github.com/mmozeiko/ed9655cf50341553d282 */
+#define STR2(x) #x
+#define STR(x) STR2(x)
+
+#ifdef __APPLE__
+#define USTR(x) "_" STR(x)
+#else
+#define USTR(x) STR(x)
+#endif
+
+#ifdef _WIN32
+#define INCBIN_SECTION ".rdata, \"dr\""
+#elif defined __APPLE__
+#define INCBIN_SECTION "__TEXT,__const"
+#else
+#define INCBIN_SECTION ".rodata"
+#endif
+
+/* this aligns start address to 16 and terminates byte array with explicit 0
+ * which is not really needed, feel free to change it to whatever you want/need
+ */
+#define INCBIN(prefix, name, file) \
+	__asm__(".section " INCBIN_SECTION "\n" \
+			".global " USTR(prefix) "_" STR(name) "_start\n" \
+			".balign 16\n" \
+			USTR(prefix) "_" STR(name) "_start:\n" \
+			".incbin \"" file "\"\n" \
+			\
+			".global " STR(prefix) "_" STR(name) "_end\n" \
+			".balign 1\n" \
+			USTR(prefix) "_" STR(name) "_end:\n" \
+			".byte 0\n" \
+	); \
+	extern __aligned(16)    const char prefix ## _ ## name ## _start[]; \
+	extern                  const char prefix ## _ ## name ## _end[];
+
+INCBIN(_bin, nrf70_fw, STR(CONFIG_NRF_WIFI_FW_BIN));
 
 enum nrf_wifi_status nrf_wifi_fw_load(void *rpu_ctx)
 {
 	enum nrf_wifi_status status = NRF_WIFI_STATUS_FAIL;
-	struct nrf_wifi_fmac_fw_info fw_info;
+	struct nrf_wifi_fmac_fw_info fw_info = { 0 };
 #if defined(CONFIG_NRF_WIFI_PATCHES_EXT_FLASH) && defined(CONFIG_NORDIC_QSPI_NOR)
 	const struct device *flash_dev = DEVICE_DT_GET(DT_INST(0, nordic_qspi_nor));
 #endif /* CONFIG_NRF_WIFI_PATCHES_EXT_FLASH */
 
-	memset(&fw_info, 0, sizeof(fw_info));
-	fw_info.lmac_patch_pri.data = (void *) nrf_wifi_lmac_patch_pri_bimg;
-	fw_info.lmac_patch_pri.size = sizeof(nrf_wifi_lmac_patch_pri_bimg);
-	fw_info.lmac_patch_sec.data = (void *) nrf_wifi_lmac_patch_sec_bin;
-	fw_info.lmac_patch_sec.size = sizeof(nrf_wifi_lmac_patch_sec_bin);
-	fw_info.umac_patch_pri.data = (void *) nrf_wifi_umac_patch_pri_bimg;
-	fw_info.umac_patch_pri.size = sizeof(nrf_wifi_umac_patch_pri_bimg);
-	fw_info.umac_patch_sec.data = (void *) nrf_wifi_umac_patch_sec_bin;
-	fw_info.umac_patch_sec.size = sizeof(nrf_wifi_umac_patch_sec_bin);
+	status = nrf_wifi_fmac_fw_parse(rpu_ctx, (const uint8_t *)_bin_nrf70_fw_start,
+					_bin_nrf70_fw_end - _bin_nrf70_fw_start, &fw_info);
+	if (status != NRF_WIFI_STATUS_SUCCESS) {
+		LOG_ERR("%s: nrf_wifi_fmac_fw_parse failed", __func__);
+		return status;
+	}
 
 #if defined(CONFIG_NRF_WIFI_PATCHES_EXT_FLASH) && defined(CONFIG_NORDIC_QSPI_NOR)
 	nrf_qspi_nor_xip_enable(flash_dev, true);
 #endif /* CONFIG_NRF_WIFI */
 	/* Load the FW patches to the RPU */
-	status = nrf_wifi_fmac_fw_load(rpu_ctx,
-				       &fw_info);
+	status = nrf_wifi_fmac_fw_load(rpu_ctx, &fw_info);
 
 	if (status != NRF_WIFI_STATUS_SUCCESS) {
 		printf("%s: nrf_wifi_fmac_fw_load failed\n", __func__);

drivers/wifi/nrf700x/CMakeLists.txt
@@ -127,3 +127,16 @@ zephyr_compile_definitions_ifdef(CONFIG_NRF700X_ON_QSPI
 	-DNRF53_ERRATA_43_ENABLE_WORKAROUND=0
 	-DNRF52_ERRATA_215_ENABLE_WORKAROUND=0
 )
+
+# RPU FW patch binaries based on the selected configuration
+zephyr_compile_definitions_ifdef(CONFIG_NRF700X_SYSTEM_MODE
+	-DCONFIG_NRF_WIFI_FW_BIN=${OS_AGNOSTIC_BASE}/fw_bins/default/nrf70.bin
+)
+
+zephyr_compile_definitions_ifdef(CONFIG_NRF700X_RADIO_TEST
+	-DCONFIG_NRF_WIFI_FW_BIN=${OS_AGNOSTIC_BASE}/fw_bins/radio_test/nrf70.bin
+)
+
+zephyr_compile_definitions_ifdef(CONFIG_NRF700X_SCAN_ONLY
+	-DCONFIG_NRF_WIFI_FW_BIN=${OS_AGNOSTIC_BASE}/fw_bins/scan_only/nrf70.bin
+)

commit eae3c094f735b97fd80e4c2921a383291255e88d

drivers: wifi: Add a workaround for XIP
XIP is broken in commit 8b73132c622("drivers: wifi: Add binary patch
download support"), after debugging found that reading a single byte
from external flash before accessing XIP address solves the issue, it is
still not root causes, but the workaround is reliable.

Issue is tracked in NCSDK-24746.

Signed-off-by: Chaitanya Tata <Chaitanya.Tata@nordicsemi.no>

drivers/wifi/nrf700x/src/fw_load.c
@@ -69,6 +69,23 @@ LOG_MODULE_DECLARE(wifi_nrf, CONFIG_WIFI_NRF700X_LOG_LEVEL);
 
 INCBIN(_bin, nrf70_fw, STR(CONFIG_NRF_WIFI_FW_BIN));
 #endif /* CONFIG_NRF_WIFI_PATCHES_BUILTIN */
+#if defined(CONFIG_NRF_WIFI_PATCHES_EXT_FLASH_XIP) && defined(CONFIG_NORDIC_QSPI_NOR)
+#include <zephyr/drivers/flash.h>
+#include <zephyr/storage/flash_map.h>
+int nrf_wifi_xip_workaround(const struct device *flash_dev)
+{
+	char dummy[1];
+	int ret;
+
+	ret = flash_read(flash_dev, 0, dummy, 1);
+	if (ret < 0) {
+		LOG_ERR("Failed to read from flash: %d", ret);
+		return ret;
+	}
+
+	return 0;
+}
+#endif /* CONFIG_NRF_WIFI_PATCHES_EXT_FLASH_XIP */
 
 #ifdef CONFIG_NRF_WIFI_PATCHES_EXT_FLASH_STORE
 #include <zephyr/drivers/flash.h>
@@ -242,6 +259,7 @@ enum nrf_wifi_status nrf_wifi_fw_load(void *rpu_ctx)
 	enum nrf_wifi_status status = NRF_WIFI_STATUS_FAIL;
 	struct nrf_wifi_fmac_fw_info fw_info = { 0 };
 #if defined(CONFIG_NRF_WIFI_PATCHES_EXT_FLASH_XIP) && defined(CONFIG_NORDIC_QSPI_NOR)
+	int ret;
 	const struct device *flash_dev = DEVICE_DT_GET(DT_INST(0, nordic_qspi_nor));
 #endif /* CONFIG_NRF_WIFI_PATCHES_EXT_FLASH_XIP */
 	uint8_t *fw_start;
@@ -257,6 +275,10 @@ enum nrf_wifi_status nrf_wifi_fw_load(void *rpu_ctx)
 
 #if defined(CONFIG_NRF_WIFI_PATCHES_EXT_FLASH_XIP) && defined(CONFIG_NORDIC_QSPI_NOR)
 	nrf_qspi_nor_xip_enable(flash_dev, true);
+	ret = nrf_wifi_xip_workaround(flash_dev);
+	if (ret < 0) {
+		return NRF_WIFI_STATUS_FAIL;
+	}
 #endif /* CONFIG_NRF_WIFI */
 
 	status = nrf_wifi_fmac_fw_parse(rpu_ctx, fw_start, fw_end - fw_start,