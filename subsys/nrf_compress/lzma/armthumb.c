6c44e744a8292fe020b7bbe2f74e515b9d3e90bd
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2024-10-21 14:41:38 +0100

    WIP

--- subsys/nrf_compress/lzma/armthumb.c
+++ subsys/nrf_compress/lzma/armthumb.c
@@ -13,12 +13,40 @@
 #include <stdio.h>
 #include "armthumb.h"
 
-void arm_thumb_filter(uint8_t *buf, uint32_t buf_size, uint32_t pos, bool compress)
+#include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(thumb, CONFIG_NRF_COMPRESS_LOG_LEVEL);
+
+void arm_thumb_filter(uint8_t *buf, uint32_t buf_size, uint32_t pos, bool compress,
+		      bool *end_part_match)
 {
 	uint32_t i = 0;
+uint32_t last_update_address = 0;
+
+LOG_ERR("start %x", (pos + i));
+
+if (pos <= 0x7f0)
+{
+LOG_ERR("wtf %02x %02x %02x %02x", buf[0x7f0], buf[0x7f1], buf[0x7f2], buf[0x7f3]);
+}
 
 	while ((i + 4) <= buf_size) {
+if (i == 0 && *end_part_match == true && (buf[i + 1] & 0xF8) == 0xF0)
+{
+LOG_ERR("hay, puchunino! %02x %02x %02x %02x", buf[i + 0], buf[i + 1], buf[i + 2], buf[i + 3]);
+}
+
+if ((pos + i) == 16380)
+{
+LOG_ERR("PRE: %02x %02x %02x %02x", buf[i + 0], buf[i + 1], buf[i + 2], buf[i + 3]);
+}
+else if ((pos + i) == 16382)
+{
+LOG_ERR("ACTUAL: %02x %02x %02x %02x", buf[i + 0], buf[i + 1], buf[i + 2], buf[i + 3]);
+}
 		if ((buf[i + 1] & 0xF8) == 0xF0 && (buf[i + 3] & 0xF8) == 0xF8) {
+LOG_ERR("pos: %x", (pos + i));
 			uint32_t dest;
 			uint32_t src = (((uint32_t)(buf[i + 1]) & 7) << 19)
 			| ((uint32_t)(buf[i + 0]) << 11)
@@ -26,6 +54,9 @@ void arm_thumb_filter(uint8_t *buf, uint32_t buf_size, uint32_t pos, bool compre
 			| (uint32_t)(buf[i + 2]);
 
 			src <<= 1;
+
+//LOG_ERR("hay, puchunino! %02x %02x %02x %02x", buf[i + 0], buf[i + 1], buf[i + 2], buf[i + 3]);
+last_update_address = i;
 
 			if (compress) {
 				dest = pos + (uint32_t)(i) + 4 + src;
@@ -38,9 +69,28 @@ void arm_thumb_filter(uint8_t *buf, uint32_t buf_size, uint32_t pos, bool compre
 			buf[i + 0] = (dest >> 11);
 			buf[i + 3] = 0xF8 | ((dest >> 8) & 0x7);
 			buf[i + 2] = (dest);
+
+if (i == 0)
+{
+LOG_ERR("now %02x %02x %02x %02x", buf[i + 0], buf[i + 1], buf[i + 2], buf[i + 3]);
+}
 			i += 2;
 		}
 
 		i += 2;
 	}
+
+LOG_ERR("end %x", (pos + i));
+
+LOG_ERR("%d vs %d = %02x %02x %02x %02x", i, buf_size, buf[buf_size - 4], buf[buf_size - 3], buf[buf_size - 2], buf[buf_size - 1]);
+	if (i == (buf_size - 2))
+{
+LOG_ERR("in heir: %d vs %d and %02x", i, last_update_address, (buf[i + 1] & 0xF8));
+	if (i > last_update_address && (buf[i + 1] & 0xF8) == 0xF0) {
+LOG_ERR("here %02x %02x at %p, pos is %d", buf[i + 0], buf[i + 1], &buf[i + 0], (pos + i));
+		*end_part_match = true;
+	} else {
+		*end_part_match = false;
+	}
+}
 }
--- subsys/nrf_compress/src/arm_thumb.c
+++ subsys/nrf_compress/src/arm_thumb.c
@@ -16,12 +16,18 @@ LOG_MODULE_REGISTER(nrf_compress_arm_thumb, CONFIG_NRF_COMPRESS_LOG_LEVEL);
 BUILD_ASSERT((CONFIG_NRF_COMPRESS_CHUNK_SIZE % 4) == 0,
 	     "CONFIG_NRF_COMPRESS_CHUNK_SIZE must be multiple of 4");
 
-static uint8_t output_buffer[CONFIG_NRF_COMPRESS_CHUNK_SIZE];
+/* Requires 2 extra bytes to allow checking cross-chunk 16-bit aligned ARM thumb instructions */
+#define EXTRA_BUFFER_SIZE 2
+
+static uint8_t output_buffer[CONFIG_NRF_COMPRESS_CHUNK_SIZE + EXTRA_BUFFER_SIZE];
+static uint8_t temp_extra_buffer[EXTRA_BUFFER_SIZE];
 static uint32_t data_position = 0;
+static bool has_extra_buffer_data = false;
 
 static int arm_thumb_init(void *inst)
 {
 	data_position = 0;
+	has_extra_buffer_data = false;
 
 	return 0;
 }
@@ -38,6 +44,7 @@ static int arm_thumb_deinit(void *inst)
 static int arm_thumb_reset(void *inst)
 {
 	data_position = 0;
+	has_extra_buffer_data = false;
 	memset(output_buffer, 0x00, sizeof(output_buffer));
 
 	return 0;
@@ -52,12 +59,65 @@ static int arm_thumb_decompress(void *inst, const uint8_t *input, size_t input_s
 				bool last_part, uint32_t *offset, uint8_t **output,
 				size_t *output_size)
 {
+	bool end_part_match = false;
+bool dec = false;
+
 	if (input_size > CONFIG_NRF_COMPRESS_CHUNK_SIZE) {
 		return -EINVAL;
 	}
 
-	memcpy(output_buffer, input, input_size);
-	arm_thumb_filter(output_buffer, input_size, data_position, false);
+	if (has_extra_buffer_data == true) {
+		/* Copy bytes from temporary holding buffer */
+		memcpy(output_buffer, temp_extra_buffer, sizeof(temp_extra_buffer));
+		memcpy(&output_buffer[sizeof(temp_extra_buffer)], input, input_size);
+LOG_ERR("input at this point is %02x %02x %02x %02x ... %02x %02x %02x %02x", output_buffer[0], output_buffer[1], output_buffer[2], output_buffer[3], output_buffer[input_size - 2], output_buffer[input_size - 1], output_buffer[input_size], output_buffer[input_size + 1]);
+end_part_match = true;
+dec = true;
+		has_extra_buffer_data = false;
+		input_size += sizeof(temp_extra_buffer);
+	} else {
+		memcpy(output_buffer, input, input_size);
+	}
+
+	arm_thumb_filter(output_buffer, input_size, data_position, false, &end_part_match);
+	data_position += input_size;
+	*offset = input_size;
+if (dec)
+{
+*offset -= 2;
+}
+	*output = output_buffer;
+	*output_size = input_size;
+
+	if (end_part_match == true && !last_part) {
+		/* Partial match at end of input, need to cut the final 2 bytes off and stash them */
+LOG_ERR("wtf copy from %d = %p for %d", (input_size - sizeof(temp_extra_buffer)), &output_buffer[(input_size - sizeof(temp_extra_buffer))], sizeof(temp_extra_buffer));
+		memcpy(temp_extra_buffer, &output_buffer[(input_size - sizeof(temp_extra_buffer))], sizeof(temp_extra_buffer));
+		has_extra_buffer_data = true;
+		*output_size -= sizeof(temp_extra_buffer);
+		data_position -= sizeof(temp_extra_buffer);
+LOG_ERR("gut we haz thing, stored %02x %02x", temp_extra_buffer[0], temp_extra_buffer[1]);
+	} else if (end_part_match) {
+LOG_ERR("final has it");
+} else {
+LOG_ERR("no has");
+}
+
+	return 0;
+}
+
+static int arm_thumb_decompress_old(void *inst, const uint8_t *input, size_t input_size,
+				bool last_part, uint32_t *offset, uint8_t **output,
+				size_t *output_size)
+{
+	if (input_size > CONFIG_NRF_COMPRESS_CHUNK_SIZE) {
+		return -EINVAL;
+	}
+
+		memcpy(output_buffer, input, input_size);
+bool crap = false;
+
+	arm_thumb_filter(output_buffer, input_size, data_position, false, &crap);
 	data_position += input_size;
 	*offset = input_size;
 	*output = output_buffer;
