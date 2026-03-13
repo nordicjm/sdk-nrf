041bf6755e03435517455828558361f3d390cf43
Jamie McCrae <jamie.mccrae@nordicsemi.no>
2026-03-13 11:06:52 +0000

    let's get DFU zip working

--- samples/nrf5340/extxip_smp_svr/__build.sh
+++ samples/nrf5340/extxip_smp_svr/__build.sh
@@ -1,1 +1,1 @@
-rm -rf  * && cmake -GNinja -DBOARD=nrf5340dk/nrf5340/cpuapp -DFILE_SUFFIX=no_network_core_directxip -DAPP_DIR=.. -DSB_CONFIG_PARTITION_MANAGER=n -DSB_CONFIG_DFU_ZIP=n ~/PM_MCUBOOT/zephyr/share/sysbuild
+rm -rf  * && cmake -GNinja -DBOARD=nrf5340dk/nrf5340/cpuapp -DFILE_SUFFIX=no_network_core_directxip -DAPP_DIR=.. -DSB_CONFIG_PARTITION_MANAGER=n ~/PM_MCUBOOT/zephyr/share/sysbuild
